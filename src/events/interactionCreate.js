f5b839455ca062a60d15a600c85835f1cee57e54
phxgg <stamatis.1702@gmail.com>
2023-05-02 20:52:08 +0300

    fix indentation

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,27 +1,27 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.InteractionCreate,
-	/**
-	 * 
-	 * @param {import('discord.js').Interaction} interaction 
-	 * @returns 
-	 */
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+  name: Events.InteractionCreate,
+  /**
+   * 
+   * @param {import('discord.js').Interaction} interaction 
+   * @returns 
+   */
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
 
-		try {
-			await command.execute(interaction);
-		} catch (err) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(err);
-		}
-	},
+    try {
+      await command.execute(interaction);
+    } catch (err) {
+      console.error(`Error executing ${interaction.commandName}`);
+      console.error(err);
+    }
+  },
 };
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,14 +1,14 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
+  name: Events.ClientReady,
+  once: true,
   /**
-	 * 
-	 * @param {import('discord.js').Client} client 
-	 * @returns 
-	 */
-	execute(client) {
-		console.log(`Logged in as ${client.user.tag}`);
-	},
+   * 
+   * @param {import('discord.js').Client} client 
+   * @returns 
+   */
+  execute(client) {
+    console.log(`Logged in as ${client.user.tag}`);
+  },
 };
