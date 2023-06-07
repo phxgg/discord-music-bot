3842700dae2ca5ee1c4a7f163f8392f4f35b411f
phxgg <stamatis.1702@gmail.com>
2023-06-07 14:26:57 +0300

    Removed express-status-monitor

--- src/index.js
+++ src/index.js
@@ -7,19 +7,3 @@ const manager = new ShardingManager(path.join(__dirname, 'app.js'), {
 });
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
-
-// Monitoring web server
-if (process.env.ENABLE_MONITORING === 'true') {
-  const express = require('express');
-  const app = express();
-
-  app.use(require('express-status-monitor')());
-
-  app.get('/', (req, res) => {
-    res.redirect('/status');
-  });
-
-  app.listen(3000, () => {
-    console.log('[Discord Music Bot] Monitoring web server: http://localhost:3000');
-  });
-}
