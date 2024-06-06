ece011bb7a5aba51488a50e29b0a3ce13b315689
phxgg <45177754+phxgg@users.noreply.github.com>
2024-06-06 21:42:24 +0300

    Dev (#14)
    * update .gitignore
    
    * chore: bump deps
    
    * chore: bump deps
    
    * chore: update readme
    
    * chore: bump deps
    
    * feat: bump deps, use other lyric mechanism
    
    * fix: lyrics and missing module
    
    * chore: minor fixes and comments

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -2,8 +2,6 @@ const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
 const { useQueue, useMainPlayer } = require('discord-player');
 const MessageType = require('../../types/MessageType');
 const createEmbedMessage = require('../../utils/createEmbedMessage');
-const { lyricsExtractor } = require('@discord-player/extractor');
-const lyricsFinder = lyricsExtractor(process.env.GENIUS_ACCESS_TOKEN);
 const logger = require('../../utils/logger');
 const { parseError } = require('../../utils/funcs');
 
@@ -25,34 +23,35 @@ module.exports = {
     try {
       let songToSearch = null;
       const query = interaction.options.getString('query');
+      const queue = useQueue(interaction.guild.id);
       if (!query) {
-        const queue = useQueue(interaction.guild.id);
         if (!queue || !queue.isPlaying()) {
           return interaction.editReply(createEmbedMessage(MessageType.Warning, 'You did not provide a song title, and the player is not playing anything.', true));
         } else {
-          songToSearch = `${queue.currentTrack.title} ${queue.currentTrack.author}`;
+          songToSearch = `${queue.currentTrack.cleanTitle} ${queue.currentTrack.author}`;
         }
       } else {
         songToSearch = query;
       }
 
-      const lyrics = await lyricsFinder.search(songToSearch).catch(() => null);
-      if (!lyrics) return interaction.editReply(createEmbedMessage(MessageType.Warning, `No lyrics found for \`${songToSearch}\`.`));
+      const lyricsResults = await player.lyrics.search({ q: songToSearch }).catch(() => null);
+      const lyrics = lyricsResults?.[0];
+      if (!lyrics || !lyrics.plainLyrics) return interaction.editReply(createEmbedMessage(MessageType.Warning, `No lyrics found for \`${songToSearch}\`.`));
 
-      const trimmedLyrics = lyrics.lyrics.substring(0, 1997);
+      const trimmedLyrics = lyrics.plainLyrics.substring(0, 1997);
 
       const embed = new EmbedBuilder()
-        .setTitle(lyrics.title)
-        .setURL(lyrics.url)
-        .setThumbnail(lyrics.thumbnail)
+        .setTitle(lyrics.trackName)
+        // .setURL(lyrics.url)
+        // .setThumbnail(lyrics.thumbnail)
         .setAuthor({
-          name: lyrics.artist.name,
-          iconURL: lyrics.artist.image,
-          url: lyrics.artist.url,
+          name: lyrics.artistName,
+          // iconURL: lyrics.artist.image,
+          // url: lyrics.artist.url,
         })
         .setDescription(trimmedLyrics.length === 1997 ? `${trimmedLyrics}...` : trimmedLyrics)
         .setFooter({
-          text: 'Powered by Genius',
+          text: 'Powered by LRCLIB',
         })
         .setColor(Colors.Yellow);
 
