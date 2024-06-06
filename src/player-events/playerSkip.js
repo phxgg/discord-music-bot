5d543b4fbc17054ba39c8f1e3b39e06011a48b03
phxgg <stamatis.1702@gmail.com>
2024-06-06 21:40:30 +0300

    chore: minor fixes and comments

--- src/player-events/playerSkip.js
+++ src/player-events/playerSkip.js
@@ -11,10 +11,13 @@ module.exports = {
   async execute(queue, track) {
     logger.info(`${queue.guild.id} -> playerSkip event`);
     // Emitted when the audio player fails to load the stream for a song
+    // EDIT: This event is emitted when queue.node.skip() is called,
+    // which means that this event is emitted when the player skips a track.
+    // For this reason, we should not be sending a warning message here.
     /**
      * @type {import('discord.js').CommandInteraction}
      */
     const metadata = queue.metadata;
-    await metadata.channel.send(createEmbedMessage(MessageType.Warning, `Skipping **[${track.title}](${track.url})** due to an issue!`));
+    // await metadata.channel.send(createEmbedMessage(MessageType.Warning, `Skipping **[${track.title}](${track.url})** due to an issue!`));
   },
 };
