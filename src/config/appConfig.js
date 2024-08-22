926eb0b889ecf2f13b7704298a6df6c1c117dcfe
phxgg <stamatis.1702@gmail.com>
2024-08-22 14:58:00 +0300

    feat: add application name in env variables

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,5 +1,5 @@
 const appConfig = {
-  name: 'Discord Music Bot',
+  name: process.env.APPLICATION_NAME || 'Discord Music Bot',
   url: 'https://github.com/phxgg/discord-music-bot',
   author: 'phxgg',
   version: '1.0.0',
