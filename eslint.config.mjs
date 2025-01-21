d5f3e1705f4a869e692080bca02904c6a295564e
phxgg <stamatis.1702@gmail.com>
2025-01-21 15:43:16 +0200

    chore: bump deps and minor eslint config changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = import.meta.dirname;
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
